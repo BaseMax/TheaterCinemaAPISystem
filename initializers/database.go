a7c663f616b91de9f5fb67bd20dced97d475984d
Max Base <MaxBaseCode@Gmail.Com>
2023-06-05 18:56:09 +0100

    Update database.go

--- initializers/database.go
+++ initializers/database.go
@@ -9,15 +9,15 @@ import (
 	"gorm.io/gorm"
 )
 
-		var DB *gorm.DB
-		func ConectToDb(){
-			var err error
-		
-			dsn :=  os.Getenv("MYSQL_DB_URL")
-			DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-			if err != nil {
-				log.Fatal("faild to coneact to database ...")
-			}else {
-				fmt.Println("connect db ...")
-			}	
-		}
+var DB *gorm.DB
+func ConectToDb(){
+	var err error
+
+	dsn :=  os.Getenv("MYSQL_DB_URL")
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("faild to coneact to database ...")
+	} else {
+		fmt.Println("connect db ...")
+	}	
+}
